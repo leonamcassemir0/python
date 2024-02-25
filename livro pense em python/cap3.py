82498ca65691aefed2bf60964b7faae40fab9a7f
leonamcassemir0 <114764927+leonamcassemir0@users.noreply.github.com>
2024-02-25 10:15:23 -0300

    atualizei o exercicio do cap3

--- livro pense em python/cap3.py
+++ livro pense em python/cap3.py
@@ -28,20 +28,111 @@ do_twice(print, 'spam')
 do_four(print, 'leonam')
 """
 
+# Duas linhas
+print("Duas linhas")
+print("--------------")
+print()
 
-def do_twice():
-    for i in range(2):
-        for i in range(2):
-            print('+', end=' ')
-            print(4*'-', end=' ')
-            print(4*" ", )
-        print('+')
-        for i in range(4):
-            print('|', 4*" ")
-    for i in range(2):
-        print('+', end=' ')
-        print(4*'-', end=' ')
+
+def do_twice(f):
+    f()
+    f()
+
+
+def do_four(f):
+    do_twice(f)
+    do_twice(f)
+
+
+def print_beam():
+    print('+ - - - -', end=' ')
+
+
+def print_post():
+    print('|        ', end=' ')
+
+
+def print_beams():
+    do_twice(print_beam)
     print('+')
 
 
-do_twice()
+def print_posts():
+    do_twice(print_post)
+    print('|')
+
+
+def print_row():
+    print_beams()
+    do_four(print_posts)
+
+
+def print_grid():
+    do_twice(print_row)
+    print_beams()
+
+
+print_grid()
+
+# Quatro colunas
+print()
+print("Quatro linhas")
+print("--------------")
+print()
+
+
+def one_four_one(f, g, h):
+    f()
+    do_four(g)
+    h()
+
+
+def print_plus():
+    print('+', end=' ')
+
+
+def print_dash():
+    print('-', end=' ')
+
+
+def print_bar():
+    print('|', end=' ')
+
+
+def print_space():
+    print(' ', end=' ')
+
+
+def print_end():
+    print()
+
+
+def nothing():
+    "do nothing"
+
+
+def print1beam():
+    one_four_one(nothing, print_dash, print_plus)
+
+
+def print1post():
+    one_four_one(nothing, print_space, print_bar)
+
+
+def print4beams():
+    one_four_one(print_plus, print1beam, print_end)
+
+
+def print4posts():
+    one_four_one(print_bar, print1post, print_end)
+
+
+def print_row():
+    one_four_one(nothing, print4posts, print4beams)
+
+
+def print_grid():
+    one_four_one(print4beams, print_row, nothing)
+
+
+print_grid()
