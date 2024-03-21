20135141a2d78926bf0de66a9b193a3b314f1a0b
leonamcassemir0 <114764927+leonamcassemir0@users.noreply.github.com>
2024-03-20 22:21:02 -0300

    update

--- livro intensivo em python/cap9/restaurant.py
+++ livro intensivo em python/cap9/restaurant.py
@@ -15,10 +15,24 @@ class Restaurant:
     def set_number_served(self, update):
         self.number_served = update
 
-    def imcrement(self, sum):
+    def increment(self, sum):
         self.number_served += sum
 
 
+class IceCreamStand(Restaurant):
+    def __init__(self, restaurante_name, cuisine_name, served, *flavors):
+        super().__init__(restaurante_name, cuisine_name, served)
+        self.flavors = flavors
+
+    def sabores(self):
+        print(list(self.flavors))
+
+
+my_Order = IceCreamStand('asder', 'werredf', 45, 'Caramelo', 'Doce de leite', 'Limao', 'Menta', 'Abacaxi')
+my_Order.describe_restaurant()
+my_Order.sabores()
+
+"""
 restaurante = Restaurant("ABC", "DEF", 30)
 # restaurante.number_served = 30
 restaurante.describe_restaurant()
@@ -28,7 +42,7 @@ restaurante.describe_restaurant()
 restaurante.imcrement(10)
 restaurante.describe_restaurant()
 
-'''
+
 restaurante = Restaurant("sdfvgbh", "sedfgh")
 restaurante.describe_restaurant()
 restaurante.open_restaurant()
@@ -36,4 +50,4 @@ restaurante.open_restaurant()
 restaurante = Restaurant("zxcvb", "qwery")
 restaurante.describe_restaurant()
 restaurante.open_restaurant()
-'''
+"""
--- livro intensivo em python/cap9/user.py
+++ livro intensivo em python/cap9/user.py
@@ -2,13 +2,11 @@ class User:
     def __init__(self, first_name, last_name, *caracteristicas):
         self.nome = first_name
         self.sobrenome = last_name
-        self.caracter = caracteristicas
     attempts = 0
 
     def describe_user(self):
         print(self.nome)
         print(self.sobrenome)
-        print(self.caracter)
         print(str(self.attempts))
 
     def increment_login_attempts(self):
@@ -21,6 +19,26 @@ class User:
         print("Seja bem-vindo!")
 
 
+class Privilege():
+    def __init__(self, elogio=['can add post', 'can delete post', 'can ban user']):
+        self.elogio = elogio
+
+    def show_privilege(self):
+        print(self.elogio)
+
+
+class Admin(User):
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        self.privileges = Privilege()
+
+
+my_User = Admin('Leonam', 'Cassemiro')
+my_User.describe_user()
+my_User.privileges.show_privilege()
+
+
+'''
 usuario = User("Leonam", "Cassemiro", "Programador", "Inteligente", "Simpatic")
 usuario.describe_user()
 usuario.greet_user()
@@ -36,7 +54,7 @@ usuario.describe_user()
 usuario.reset_login_attempts()
 usuario.describe_user()
 
-'''
+
 usuario = User("sdfghj", "zsxdcfvgb", "Programador", "Inteligente", "Simpatic")
 usuario.describe_user()
 usuario.greet_user()
