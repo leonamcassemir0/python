e830f2a2b35d938668f4688d662086d55ae0d83c
leonamcassemir0 <114764927+leonamcassemir0@users.noreply.github.com>
2024-03-23 15:54:03 -0300

    acabei cap9

--- livro intensivo em python/cap9/user.py
+++ livro intensivo em python/cap9/user.py
@@ -1,39 +1,7 @@
-class User:
-    def __init__(self, first_name, last_name, *caracteristicas):
-        self.nome = first_name
-        self.sobrenome = last_name
-    attempts = 0
-
-    def describe_user(self):
-        print(self.nome)
-        print(self.sobrenome)
-        print(str(self.attempts))
-
-    def increment_login_attempts(self):
-        self.attempts += 1
-
-    def reset_login_attempts(self):
-        self.attempts = 0
-
-    def greet_user(self):
-        print("Seja bem-vindo!")
+import modulo_admin
 
 
-class Privilege():
-    def __init__(self, elogio=['can add post', 'can delete post', 'can ban user']):
-        self.elogio = elogio
-
-    def show_privilege(self):
-        print(self.elogio)
-
-
-class Admin(User):
-    def __init__(self, first_name, last_name):
-        super().__init__(first_name, last_name)
-        self.privileges = Privilege()
-
-
-my_User = Admin('Leonam', 'Cassemiro')
+my_User = modulo_admin.Admin('Leonam', 'Cassemiro')
 my_User.describe_user()
 my_User.privileges.show_privilege()
 
