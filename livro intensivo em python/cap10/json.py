d5e03db56ebcc0268ef61285820cefd5f848c73c
leonamcassemir0 <114764927+leonamcassemir0@users.noreply.github.com>
2024-04-01 21:33:57 -0300

    finish cap10

--- livro intensivo em python/cap10/json.py
+++ livro intensivo em python/cap10/json.py
@@ -1,6 +1,5 @@
 import json
 
 numbers = [2, 5, 7, 9, 2, 3, 6, 13]
-filename = 'cap10/numbers.json'
-with open(filename, 'w') as f_obj:
+with open('cap10/numbers.json', 'w') as f_obj:
     json.dump(numbers, f_obj)
